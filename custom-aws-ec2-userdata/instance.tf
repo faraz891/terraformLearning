a287b0dc7b28404e303607b28047844a5de28edc
faraz891 <8448574+faraz891@users.noreply.github.com>
2022-06-03 12:20:38 +0300

    Update instance.tf

--- custom-aws-ec2-userdata/instance.tf
+++ custom-aws-ec2-userdata/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "r100c96" {
   ami           = var.aws_instance_my_infra_ami
   instance_type = "t2.micro"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-west-2b"
   key_name      = "aws-exam-testing"
   user_data = file("installcommand.sh")
   tags = {

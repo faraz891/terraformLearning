39d98bab44f56d620f4b175f8e4efde85a713e99
faraz891 <8448574+faraz891@users.noreply.github.com>
2022-06-03 17:59:00 +0300

    Update providers.tf

--- custom-aws-ec2-userdata/providers.tf
+++ custom-aws-ec2-userdata/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "aws" {
-  profile = "default"
+  profile = "root"
   region  = var.region
   access_key = "AKIA5RKSHD3OQSV24IJV"
   secret_key = "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"
